d690f122cf1626a2b7bc717fe1b1c10043ab7a6c
Rahul Somasunderam <rahul.som@gmail.com>
2015-03-06 07:27:42 -0800

    Added support for langage validation and continuous compilation

--- src/main/groovy/numserve/NumberHelper.groovy
+++ src/main/groovy/numserve/NumberHelper.groovy
@@ -1,20 +1,36 @@
 package numserve
 
 import com.ibm.icu.text.RuleBasedNumberFormat
-import com.ibm.icu.util.ULocale
 import groovy.transform.Memoized
+import groovy.util.logging.Slf4j
 
 /**
  * Created by rahulsomasunderam on 04/03/15.
  */
 @Singleton
+@Slf4j
 class NumberHelper {
+
+  static validFormats = '''af am ar az be bg bs ca cs cy da de ee el en eo es es_419 es_AR es_BO es_CL es_CO es_CR
+      es_CU es_DO es_EC es_GT es_HN es_MX es_NI es_PA es_PE es_PR es_PY es_SV es_US es_UY es_VE et fa fa_AF fi fil
+      fo fr fr_BE fr_CH ga he hi hr hu hy id is it ja ka kl km ko ky lo lt lv mk ms mt nb nci nl nn pl pt pt_AO
+      pt_GW pt_MO pt_MZ pt_PT pt_ST pt_TL ro ru se sk sl sq sr sr_Latn sv ta th tr uk vi zh zh_Hant zh_Hant_HK
+  '''.replaceAll(' +', ' ').split(' ')
 
   @Memoized
   RuleBasedNumberFormat getNumberFormat(String language) {
-    try {
-      new RuleBasedNumberFormat(ULocale.forLanguageTag(language), RuleBasedNumberFormat.SPELLOUT)
-    } catch (Exception e) {
+    if (validFormats.contains(language)) {
+      try {
+        def locale = Locale.forLanguageTag(language)
+        def numberFormat = new RuleBasedNumberFormat(locale, RuleBasedNumberFormat.SPELLOUT)
+        log.info "Created Formatter for '${language}'"
+        return numberFormat
+      } catch (Exception e) {
+        log.warn "Could not create Formatter for '${language}'"
+        null
+      }
+    } else {
+      log.warn "'${language}' is not a valid language code"
       null
     }
   }
