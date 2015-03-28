d26a31c1bea22495afa0732d3d57c0afcf8adc5d
Rahul Somasunderam <rahul.som@gmail.com>
2015-03-28 16:12:06 -0700

    Added pure functional sample

--- src/test/groovy/util/DataSource.groovy
+++ src/test/groovy/util/DataSource.groovy
@@ -3,10 +3,10 @@ package util
  * Created by rahul on 3/6/15.
  */
 class DataSource {
-  static getReader() {
+  static Reader getReader() {
     DataSource.classLoader.getResourceAsStream('input.10.csv').newReader()
   }
-  static getLines() {
+  static List<String> getLines() {
     DataSource.classLoader.getResourceAsStream('input.10.csv').readLines()
   }
 }
